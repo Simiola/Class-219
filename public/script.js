9b204a6d412b1681c56e235f896435b1853157ef
Simiola <70043967+Simiola@users.noreply.github.com>
2023-05-21 17:52:14 -0500

    Add files via upload

---
+++ public/script.js
@@ -0,0 +1,73 @@
+const socket = io("/");
+
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "443",
+});
+
+const user = prompt("Enter your name");
+const myvideo= document.createElement("video")
+myvideo.muted=true
+
+var mystream
+
+navigator.mediaDevices.getUserMedia({
+    audio:true,
+    video:true
+})
+.then((f)=>{
+    console.log("what is f: ",f)
+    mystream=f
+    addVideoStream(myvideo,f)
+})
+
+function addVideoStream(v,s){
+    v.srcObject=s
+    v.addEventListener("loadedmetadata",()=>{
+        v.play()
+        $("#video_stream").append(v)
+    })
+}
+
+$(function () {
+    $("#show_chat").click(function () {
+        $(".left-window").css("display", "none")
+        $(".right-window").css("display", "block")
+        $(".header_back").css("display", "block")
+    })
+    $(".header_back").click(function () {
+        $(".left-window").css("display", "block")
+        $(".right-window").css("display", "none")
+        $(".header_back").css("display", "none")
+    })
+
+    $("#send").click(function () {
+        if ($("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+    $("#chat_message").keydown(function (e) {
+        if (e.key == "Enter" && $("#chat_message").val().length !== 0) {
+            socket.emit("message", $("#chat_message").val());
+            $("#chat_message").val("");
+        }
+    })
+
+})
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+});
+
+socket.on("createMessage", (message, userName) => {
+    $(".messages").append(`
+        <div class="message">
+            <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName
+        }</span> </b>
+            <span>${message}</span>
+        </div>
+    `)
+});
